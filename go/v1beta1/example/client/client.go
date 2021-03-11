f9e57c99a0cea6e38658d36f70aaf2a0cc542615
Ahmed Alsabag <ahmed.alsabag@gmail.com>
2021-03-11 11:31:42 -0600

    initial structs for further sonar data

--- go/v1beta1/example/client/client.go
+++ go/v1beta1/example/client/client.go
@@ -42,13 +42,27 @@ func main() {
 
 }
 
+func retrieveSonarServerInfo(sonarUrl string, staticanalysis *pb.Occurrence_StaticAnalysis) {
+
+}
+
+func retrieveOverallMeasures(sonarUrl string, staticanalysis *pb.Occurrence_StaticAnalysis) {
+
+}
+
+func retrieveAllIssues(sonarUrl string, staticanalysis *pb.Occurrence_StaticAnalysis) {
+	// TODO implement pagination
+}
+
 func handleWebhook(w http.ResponseWriter, request *http.Request) {
-	// event := &Event{}
-	// if err := json.NewDecoder(request.Body).Decode(event); err != nil {
-	// 	w.WriteHeader(http.StatusInternalServerError)
-	// 	fmt.Println("error reading webhook event")
-	// 	return
-	// }
+	event := &Event{}
+	if err := json.NewDecoder(request.Body).Decode(event); err != nil {
+		w.WriteHeader(http.StatusInternalServerError)
+		fmt.Println("error reading webhook event")
+		return
+	}
+	body, _ := json.Marshal(event)
+	log.Printf(string(body))
 
 	staticanalysis := &pb.Occurrence_StaticAnalysis{
 		StaticAnalysis: &static_analysis_go_proto.Details{
@@ -154,4 +168,163 @@ func handleWebhook(w http.ResponseWriter, request *http.Request) {
 	}
 	out, _ := json.Marshal(occ)
 	log.Printf(string(out))
+}
+
+// Sonar stuff-----------------------------------------------------------
+type Event struct {
+	TaskID      string            `json:"taskid"`
+	Status      string            `json:"status"`
+	AnalyzedAt  string            `json:"analyzedat"`
+	GitCommit   string            `json:"revision"`
+	Project     *Project          `json:"project"`
+	QualityGate *QualityGate      `json:"qualityGate"`
+	Branch      *Branch           `json:"branch"`
+	Properties  map[string]string `json:"properties"`
+}
+
+// Branch is...
+type Branch struct {
+	Name   string `json:"name"`
+	Type   string `json:"type"`
+	IsMain bool   `json:"isMain"`
+	URL    string `json:"url"`
+}
+
+// Project is
+type Project struct {
+	Key  string `json:"key"`
+	Name string `json:"name"`
+	URL  string `json:"url"`
+}
+
+// QualityGate is...
+type QualityGate struct {
+	Conditions []*Condition `json:"conditions"`
+	Name       string       `json:"name"`
+	Status     string       `json:"status"`
+}
+
+// Condition is...
+type Condition struct {
+	ErrorThreshold string `json:"errorThreshold"`
+	Metric         string `json:"metric"`
+	OnLeakPeriod   bool   `json:"onLeakPeriod"`
+	Operator       string `json:"operator"`
+	Status         string `json:"status"`
+}
+
+// Overall Measures
+type OverallMeasures struct {
+	Component struct {
+		ID        string `json:"id"`
+		Key       string `json:"key"`
+		Name      string `json:"name"`
+		Qualifier string `json:"qualifier"`
+		Measures  []struct {
+			Metric    string `json:"metric"`
+			Value     string `json:"value,omitempty"`
+			BestValue bool   `json:"bestValue,omitempty"`
+			Periods   []struct {
+				Index     int    `json:"index"`
+				Value     string `json:"value"`
+				BestValue bool   `json:"bestValue"`
+			} `json:"periods,omitempty"`
+			Period struct {
+				Index     int    `json:"index"`
+				Value     string `json:"value"`
+				BestValue bool   `json:"bestValue"`
+			} `json:"period,omitempty"`
+		} `json:"measures"`
+	} `json:"component"`
+	Metrics []struct {
+		Key                   string `json:"key"`
+		Name                  string `json:"name"`
+		Description           string `json:"description"`
+		Domain                string `json:"domain"`
+		Type                  string `json:"type"`
+		HigherValuesAreBetter bool   `json:"higherValuesAreBetter,omitempty"`
+		Qualitative           bool   `json:"qualitative"`
+		Hidden                bool   `json:"hidden"`
+		Custom                bool   `json:"custom"`
+		BestValue             string `json:"bestValue,omitempty"`
+		DecimalScale          int    `json:"decimalScale,omitempty"`
+		WorstValue            string `json:"worstValue,omitempty"`
+	} `json:"metrics"`
+	Periods []struct {
+		Index int    `json:"index"`
+		Mode  string `json:"mode"`
+		Date  string `json:"date"`
+	} `json:"periods"`
+}
+
+// Individual Issues
+type Issues struct {
+	Total  int `json:"total"`
+	P      int `json:"p"`
+	Ps     int `json:"ps"`
+	Paging struct {
+		PageIndex int `json:"pageIndex"`
+		PageSize  int `json:"pageSize"`
+		Total     int `json:"total"`
+	} `json:"paging"`
+	EffortTotal int `json:"effortTotal"`
+	DebtTotal   int `json:"debtTotal"`
+	Issues      []struct {
+		Key       string `json:"key"`
+		Rule      string `json:"rule"`
+		Severity  string `json:"severity"`
+		Component string `json:"component"`
+		Project   string `json:"project"`
+		Line      int    `json:"line"`
+		Hash      string `json:"hash"`
+		TextRange struct {
+			StartLine   int `json:"startLine"`
+			EndLine     int `json:"endLine"`
+			StartOffset int `json:"startOffset"`
+			EndOffset   int `json:"endOffset"`
+		} `json:"textRange"`
+		Flows        []interface{} `json:"flows"`
+		Status       string        `json:"status"`
+		Message      string        `json:"message"`
+		Effort       string        `json:"effort"`
+		Debt         string        `json:"debt"`
+		Tags         []string      `json:"tags"`
+		Transitions  []interface{} `json:"transitions"`
+		Actions      []interface{} `json:"actions"`
+		Comments     []interface{} `json:"comments"`
+		CreationDate string        `json:"creationDate"`
+		UpdateDate   string        `json:"updateDate"`
+		Type         string        `json:"type"`
+		Organization string        `json:"organization"`
+		Scope        string        `json:"scope"`
+	} `json:"issues"`
+	Components []struct {
+		Organization string `json:"organization"`
+		Key          string `json:"key"`
+		UUID         string `json:"uuid"`
+		Enabled      bool   `json:"enabled"`
+		Qualifier    string `json:"qualifier"`
+		Name         string `json:"name"`
+		LongName     string `json:"longName"`
+		Path         string `json:"path,omitempty"`
+	} `json:"components"`
+	Rules []struct {
+		Key      string `json:"key"`
+		Name     string `json:"name"`
+		Lang     string `json:"lang"`
+		Status   string `json:"status"`
+		LangName string `json:"langName"`
+	} `json:"rules"`
+	Users     []interface{} `json:"users"`
+	Languages []struct {
+		Key  string `json:"key"`
+		Name string `json:"name"`
+	} `json:"languages"`
+	Facets []struct {
+		Property string `json:"property"`
+		Values   []struct {
+			Val   string `json:"val"`
+			Count int    `json:"count"`
+		} `json:"values"`
+	} `json:"facets"`
 }
